fa6baee8c7f7c9276e99b478007f753032aa9963
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2024-04-30 11:47:51 +0900

    added

---
+++ lib/components/screen_topbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class ScreenTopBar extends StatelessWidget{
+  const ScreenTopBar({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+
+      ],
+    );
+  }
+}
---
+++ lib/pages/MainScreenPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class MainScreenPage extends StatefulWidget{
+  const MainScreenPage({super.key});
+  _MainScreenPageState createState()=>_MainScreenPageState();
+}
+class _MainScreenPageState extends State<MainScreenPage>{
+  @override
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context){
+    return Text("Test");
+  }
+}
